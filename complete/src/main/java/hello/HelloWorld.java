49d1a2dc5d843d9f061de4e9f8c167abde82bdd0
Boriphuth Saensukphattraka <boriphuth.sa@gmail.com>
2019-07-25 15:49:13 +0700

    update code smell

--- complete/src/main/java/hello/HelloWorld.java
+++ complete/src/main/java/hello/HelloWorld.java
@@ -1,13 +1,15 @@
 package hello;
 
+import java.util.logging.Logger;
+
 import org.joda.time.LocalTime;
 
 public class HelloWorld {
   public static void main(String[] args) {
     LocalTime currentTime = new LocalTime();
-    System.out.println("The current local time is: " + currentTime);
+    Logger.getLogger("The current local time is: " + currentTime);
 
     Greeter greeter = new Greeter();
-    System.out.println(greeter.sayHello());
+    Logger.getLogger(greeter.sayHello());
   }
 }
